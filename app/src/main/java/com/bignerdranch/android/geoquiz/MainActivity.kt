835d82a8ca9b6ccd2d8a0a9899e58f51c5d3a22f
chd-k <asgribovskaya@yandex.ru>
2022-06-21 00:05:30 +0300

    Feat(MainActivity): add snackbar instead of toasts Fix(activity_main): change layout margin for buttons

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -4,7 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,18 +20,18 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
 
         trueButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT
+            Snackbar.make(
+                view,
+                R.string.correct_snackbar,
+                BaseTransientBottomBar.LENGTH_SHORT
             ).show()
         }
 
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT
+            Snackbar.make(
+                view,
+                R.string.incorrect_snackbar,
+                BaseTransientBottomBar.LENGTH_SHORT
             ).show()
         }
     }
