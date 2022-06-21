2b024787259b9cab652a896481bf519fb8b84932
chd-k <asgribovskaya@yandex.ru>
2022-06-22 00:57:59 +0300

    Feat(MainActivity): add log messages and grades calculation, make buttons not clickable after answer

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -2,9 +2,13 @@ package com.bignerdranch.android.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
 import com.bignerdranch.android.geoquiz.databinding.ActivityMainBinding
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,9 +23,11 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_asia, true),
     )
     private var currentIndex = 0
+    private val answersMap = mutableMapOf<Int, Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -40,9 +46,35 @@ class MainActivity : AppCompatActivity() {
         updateQuestion()
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         binding.questionTextView.setText(questionTextResId)
+        setButtonsClickable(true)
     }
 
     private fun nextQuestion() {
@@ -57,10 +89,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = questionBank[currentIndex].answer
-        val messageResId = if (userAnswer == correctAnswer) {
-            R.string.correct_snackbar
+        val messageResId: Int
+        if (userAnswer == correctAnswer) {
+            messageResId = R.string.correct_snackbar
+            answersMap[questionBank[currentIndex].textResId] = 1
         } else {
-            R.string.incorrect_snackbar
+            messageResId = R.string.incorrect_snackbar
+            answersMap[questionBank[currentIndex].textResId] = 0
         }
 
         Snackbar.make(
@@ -68,5 +103,25 @@ class MainActivity : AppCompatActivity() {
             messageResId,
             BaseTransientBottomBar.LENGTH_SHORT
         ).show()
+
+        setButtonsClickable(false)
+
+        checkGrades()
+    }
+
+    private fun checkGrades() {
+        if (answersMap.size == questionBank.size) {
+            Toast.makeText(
+                this,
+                "Your score ${100 * answersMap.values.sum() / questionBank.size} %!",
+                Toast.LENGTH_SHORT
+            ).show()
+            answersMap.clear()
+        }
+    }
+
+    private fun setButtonsClickable(clickable: Boolean) {
+        binding.trueButton.isClickable = clickable
+        binding.falseButton.isClickable = clickable
     }
 }
